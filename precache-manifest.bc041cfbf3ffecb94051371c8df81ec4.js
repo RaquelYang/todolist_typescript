b21a3d28cd9963e250e7201137e451282f9251ac
RaquelYang <72677059+RaquelYang@users.noreply.github.com>
2022-03-19 02:40:22 +0000

    Deploying to gh-pages from @ RaquelYang/todolist_typescript@4fbfd4d534754892a5ccf6928bbae9a1dfcac9d6 🚀

---
+++ precache-manifest.bc041cfbf3ffecb94051371c8df81ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dee2778d214685707e",
+    "url": "css/app.7aa40c95.css"
+  },
+  {
+    "revision": "3d766701dcf6f347f8cb9a3034c27ff9",
+    "url": "img/pen.png"
+  },
+  {
+    "revision": "36fe469ac6888fd62437dd7e68de463d",
+    "url": "index.html"
+  },
+  {
+    "revision": "41dee2778d214685707e",
+    "url": "js/app.de80602d.js"
+  },
+  {
+    "revision": "9319cf3e565e986a4a96",
+    "url": "js/chunk-vendors.e1e449f4.js"
+  },
+  {
+    "revision": "ddee61204be9a6aefff6d2733600b2a2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
