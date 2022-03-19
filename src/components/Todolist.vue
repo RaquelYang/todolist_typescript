4fbfd4d534754892a5ccf6928bbae9a1dfcac9d6
RaquelYang <lorehmmoreh007@gmail.com>
2022-03-19 10:39:16 +0800

    add pwa

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -24,10 +24,14 @@
         >
           <span aria-hidden="true">&times;</span>
         </button>
-        <button type="button" class="close mr-5" data-dismiss="modal" aria-label="Close"
+        <button type="button" class="close mr-2" data-dismiss="modal" aria-label="Close"
           @click.stop="editTodo(todo)"
         >
-          <span aria-hidden="true">&#200;</span>
+          <span aria-hidden="true">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+            </svg>
+          </span>
         </button>
       </li>
     </ul>
@@ -44,50 +48,48 @@ class Todo {
     this.todo = todo;
     this.checked = false;
   }
-  
 }
 
 @Component
 export default class Todolist extends Vue {
   todoInput: string = '';
   todoList: Todo[] = [];
-
   toggleChecked(index: number) {
-    this.todoList[index].checked = !this.todoList[index].checked
+    this.todoList[index].checked = !this.todoList[index].checked;
   }
 
   save(){
-    window.localStorage.setItem('typeScriptTodos', JSON.stringify(this.todoList))
+    window.localStorage.setItem('typeScriptTodos', JSON.stringify(this.todoList));
   }
 
   addTodo(){
-    console.log('TodoInput',this.todoInput);
-    let todo = new Todo(this.todoInput)
-    this.todoList.push(todo)
-    this.todoInput = ''
-    this.save()
+    // console.log('TodoInput',this.todoInput);
+    let todo = new Todo(this.todoInput);
+    this.todoList.push(todo);
+    this.todoInput = '';
+    this.save();
   }
 
-  delTodo(todo:Todo){
-    console.log('delTodo',todo);
+  delTodo(todo: Todo) {
+    // console.log('delTodo',todo);
     this.todoList = this.todoList.filter((todoItem)=>{
       return todoItem !== todo;
     })
-    this.save()
+    this.save();
   }
-  editTodo(todo:Todo){
-    console.log(todo);
-    let content = window.prompt('請輸入修改內容')
-    console.log(content);
-    if(content?.trim()){
-      todo.todo = content.trim()
-      this.save()
+  editTodo(todo: Todo) {
+    // console.log(todo);
+    const content = window.prompt('請輸入修改內容')
+    // console.log(content);
+    if (content?.trim()) {
+      todo.todo = content.trim();
+      this.save();
     }
   }
-  mounted(){
-    const store = window.localStorage.getItem('typeScriptTodos')
-    if(store!==null){
-      this.todoList = JSON.parse(store) || []
+  mounted() {
+    const store = window.localStorage.getItem('typeScriptTodos');
+    if (store !== null) {
+      this.todoList = JSON.parse(store) || [];
     }
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,96 @@
 module.exports = {
   // 設定打包後的路徑
-  publicPath: './',
+  publicPath: "./",
   // 修改 webpack 設定，改 title
-  chainWebpack (config) {
-    config.plugin('html').tap(args => {
-      args[0].title = 'Todolist'
-      return args
-    })
-  }
-}
+  chainWebpack(config) {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Todolist";
+      return args;
+    });
+  },
+  pwa: {
+    manifestOptions: {
+      name: "Todolist",
+      shortname: "Todolist",
+      background_color: "#333",
+      theme_color: "#333",
+      scope: "./",
+      start_url: "./",
+      display: "standalone",
+      icons: [
+        {
+          src: "./img/icons/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/android-chrome-maskable-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-60x60.png",
+          sizes: "60x60",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-76x76.png",
+          sizes: "76x76",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-120x120.png",
+          sizes: "120x120",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon-180x180.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/apple-touch-icon.png",
+          sizes: "180x180",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/msapplication-icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: "./img/icons/mstile-150x150.png",
+          sizes: "150x150",
+          type: "image/png",
+        },
+      ],
+    },
+  },
+};
